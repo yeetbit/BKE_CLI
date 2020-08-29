1d56d4888cc2277fec4687a93d8be5afa82c3365
Obi leppy <return0000000@gmail.com>
2020-08-29 11:30:15 +0200

    Figuring oponent pattern

--- src/gamelogic/player/Player_ai.java
+++ src/gamelogic/player/Player_ai.java
@@ -1,9 +1,19 @@
 package gamelogic.player;
 
 public class Player_ai extends Player {
+    private boolean turn;
 
     public Player_ai() {
+        this.turn = false;
+        
+    }
 
+    @Override
+    public void isTurn() {
+        // TODO Auto-generated method stub
+        super.isTurn();
+        
+        
     }
 
     
