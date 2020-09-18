19ea5053190b8abcee85de735922c51791c2d435
Obi <return0000000@gmail.com>
2020-09-18 19:01:32 +0200

    added: info

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Game game = new Game(true,false);
+        Game game = new Game(true,false);// for 2 players change to true true, for 1 player change to true false.
         game.setDefaultPlayDek();
         game.startGame();       
         
